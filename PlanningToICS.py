684ed88c3e7685c8e09505aef943eb12da08565a
Valentin68 <valentin.mercy.vm@gmail.com>
2020-09-04 17:27:25 +0200

    fixed issue relative to periods not starting on monday

--- PlanningToICS.py
+++ PlanningToICS.py
@@ -23,60 +23,74 @@ from ics import Calendar, Event
 import re
 import pandas as pd
 from datetime import datetime, timedelta
-from os import path
+from os import path, listdir
+import csv
 
 frStrings = {
-'welcome' : "Bienvenue sur l'utilitaire UTBMPlanningToICS !\nCelui-ci te permet de convertir ton emploi du temps consultable à chaque début de semestre sur uPortal, en un agenda numérique au format standard ICS. En important ce dernier sur Google Agenda ou iCalendar (par exemple), tu pourras consulter tes horaires et salles de cours beaucoup plus facilement (sur ton PC, ton smartphone, etc...). Fini la corvée de la saisie de l'emploi du temps à chaque début de semestre !\nCommence par saisir le nom du fichier txt (avec l'extension .txt) dans lequel tu as copié-collé ton emploi du temps uPortal (lire le README.md maintenant si ce n'est pas déjà fait). Ce fichier doit se trouver dans le répertoire racine du projet !", 
-'askfile' : "Nom du fichier ? : ",
-'tooshort' :   "Erreur : le nom de fichier saisi est trop court",
-'wrongextension' : "Erreur : le nom de fichier saisi ne porte pas l'extension .txt",
-'notfound' : "Erreur : le fichier \"{}\" est introuvable",
-'readsuccess' : "Fichier lu avec succes",
-'errline' : "Erreur de format ligne {}",
-'requestreview' : "Merci de vérifier le fichier et de recommencer",
-'wrongheader' : "Erreur de format de l'entete du fichier",
-'introaskgroup' : "Merci de saisir tes différents groupes pour les cours non-hebdomadaires (information à trouver dans le tableau des affectations recu en début de semestre, juste après les inscriptions pédagogiques)",
-'askgroup'  : "Saisis ton groupe (A/B) pour le cours suivant : {}",
-'errorAB' : "Groupe invalide, saisis A ou B : "
+    'welcome': "Bienvenue sur l'utilitaire UTBMPlanningToICS !\nCelui-ci te permet de convertir ton emploi du temps consultable à chaque début de semestre sur uPortal, en un agenda numérique au format standard ICS. En important ce dernier sur Google Agenda ou iCalendar (par exemple), tu pourras consulter tes horaires et salles de cours beaucoup plus facilement (sur ton PC, ton smartphone, etc...). Fini la corvée de la saisie de l'emploi du temps à chaque début de semestre !\nCommence par saisir le nom du fichier txt (avec l'extension .txt) dans lequel tu as copié-collé ton emploi du temps uPortal (lire le README.md maintenant si ce n'est pas déjà fait). Ce fichier doit se trouver dans le répertoire racine du projet !",
+    'askfile': "Nom du fichier ? : ",
+    'tooshort':   "Erreur : le nom de fichier saisi est trop court",
+    'wrongextension': "Erreur : le nom de fichier saisi ne porte pas l'extension .txt",
+    'notfound': "Erreur : le fichier \"{}\" est introuvable",
+    'readsuccess': "Fichier lu avec succes",
+    'errline': "Erreur de format ligne {}",
+    'requestreview': "Merci de vérifier le fichier et de recommencer",
+    'wrongheader': "Erreur de format de l'entete du fichier",
+    'introaskgroup': "Merci de saisir tes différents groupes pour les cours non-hebdomadaires (information à trouver dans le tableau des affectations recu en début de semestre, juste après les inscriptions pédagogiques)",
+    'askgroup': "Saisis ton groupe (A/B) pour le cours suivant : {}",
+    'errorAB': "Groupe invalide, saisis A ou B : ",
+    'noSemesterFile': "Erreur : aucun calendrier semestriel n'a été trouvé dans le répertoire courant. Il est toutefois possible d'en créer un en suivant le modèle donnée dans le README. Merci de recommencer une fois le fichier créé.",
+    'severalSemCals': "Plusieurs calendriers semestriels ont été trouvés : {}. Merci de sélectionner celui à considérer en saisissant le code correspondant (A20 par exemple) : ",
+    'wrongCode': "La saisie ne correspond pas à un code de semestre. Recommence : ",
+    'errorPeriods' : "Erreur dans le calendrier du semestre : au moins une période est invalide (début>fin) ou porte un type erroné (différent de A/B)",
+    'overlappingPeriods' : "Erreur dans le calendrier du semestre : au moins deux périodes se chevauchent"
 }
 
 enStrings = {
-'welcome' : "Welcome to the UTBMPlanningToICS utility! \nThis utility allows you to convert your schedule, which can be consulted at the start of each semester on uPortal, into a digital agenda in the standard ICS format. By importing it on Google Calendar or iCalendar (for example), you will be able to consult your scheduled classes and classrooms much more easily (on your PC, your smartphone, etc ...). No more drudgery of entering the schedule at the start of each semester!\nPlease start by typing the full filename (with .txt extension) in which you pasted your uPortal planning (see the README.md before continuing). This file must be placed in the same root folder as PlanningToICS.py !",
-'askfile' : "Filename ? : ",
-'tooshort' :   "Error : given filename is too short",
-'wrongextension' : "Error : given filename does not have the extension .txt",
-'notfound' : "Error : no file named \"{}\" inside current directory",
-'readsuccess' : "File read with success",
-'errline' : " - Formatting error on line {}",
-'requestreview' : "Please review input file and retry",
-'wrongheader' : "Error in file header (first line is not correctly formatted)",
-'introaskgroup' : "Please enter your different groups for non-weekly classes (these informations can be found in the groups table that you get at the beginning of each semester after your UVs registrations)",
-'askgroup' : "Enter your week group (A/B) for the following course : {}",
-'errorAB' : "Invalid group, please enter A or B : "
+    'welcome': "Welcome to the UTBMPlanningToICS utility! \nThis utility allows you to convert your schedule, which can be consulted at the start of each semester on uPortal, into a digital agenda in the standard ICS format. By importing it on Google Calendar or iCalendar (for example), you will be able to consult your scheduled classes and classrooms much more easily (on your PC, your smartphone, etc ...). No more drudgery of entering the schedule at the start of each semester!\nPlease start by typing the full filename (with .txt extension) in which you pasted your uPortal planning (see the README.md before continuing). This file must be placed in the same root folder as PlanningToICS.py !",
+    'askfile': "Filename ? : ",
+    'tooshort':   "Error : given filename is too short",
+    'wrongextension': "Error : given filename does not have the extension .txt",
+    'notfound': "Error : no file named \"{}\" inside current directory",
+    'readsuccess': "File read with success",
+    'errline': " - Formatting error on line {}",
+    'requestreview': "Please review input file and retry",
+    'wrongheader': "Error in file header (first line is not correctly formatted)",
+    'introaskgroup': "Please enter your different groups for non-weekly classes (these informations can be found in the groups table that you get at the beginning of each semester after your UVs registrations)",
+    'askgroup': "Enter your week group (A/B) for the following course : {}",
+    'errorAB': "Invalid group, please enter A or B : ",
+    'noSemesterFile': "Error : no semester calendar was found. Please consider creating it by following the structure given in README file, and try again.",
+    'severalSemCals': "Several semester calendars were found : {}. Please select the one to consider by entering the corresponding code (A20 for example) : ",
+    'wrongCode': "Given string does not match a semester code. Please try again : ",
+    'errorPeriods' : "Error in semester calendar : at least one period is erroneous (start>end) or has an invalid type (different from A/B)",
+    'overlappingPeriods' : "Error in semester calendar : at least two periods are overlapping"
 }
 
-daysOfWeekFR = ['lundi','mardi','mercredi','jeudi','vendredi','samedi']
-daysOfWeekEN = ['monday','tuesday','wednesday','thursday','friday','saturday']
+daysOfWeekFR = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi']
+daysOfWeekEN = ['monday', 'tuesday', 'wednesday',
+                'thursday', 'friday', 'saturday']
 
 daysOfWeek = []
 
-'''
-e.name = "Test PythonToICS"
-e.begin = start_date.strftime("%Y-%m-%d %H:%M:%S")
-e.duration = Duration
-e.description = "Ceci est la description de cet evenement test"
+regex_dict = {
+    'firstline': '^UV\s*Groupe\s*Jour\s*Début\s*Fin\s*Fréquence\s*Salle\(s\)\s*$',
+    'middleline': '^\s*[A-Z0-9]{4}\s+([A-Z]\s+){0,1}(CM|TD|TP)\s[0-9]{1,2}\s+(lundi|mardi|mercredi|jeudi|vendredi|samedi)\s+([0-9]{1,2}:[0-9]{1,2}\s+){2}[1-2]\s+[A-Z]\s[0-9a-z]{3,4}\s*$',
+    'semesterFile': '^SEM_(A|P)[0-9]{2}.csv$'
+}
 
-c.events.add(e)
-'''
+regex = {}  # dict containing compiled regexs
+for key in regex_dict:
+    regex[key] = re.compile(regex_dict[key])
 
 def checkFileExistence(filename):
     return path.exists(filename)
 
+
 def validateFile(filename):
-    if len(filename)<5:
+    if len(filename) < 5:
         print(VerbList['tooshort'])
         return False
-    if filename[-4:]!=".txt":
+    if filename[-4:] != ".txt":
         print(VerbList['wrongextension'])
         return False
     if not checkFileExistence(filename):
@@ -84,81 +98,164 @@ def validateFile(filename):
         return False
     return True
 
+
 def askGroups(classes):
     Groups = []
     print(VerbList['introaskgroup'])
     for Class in classes:
-        if Class[5]>1:
-            group = input(" - "+VerbList['askgroup'].format(Class[0]+" ("+Class[1]+")"+" | "+daysOfWeek[Class[2]].capitalize()+" "+Class[3]+" -> "+Class[4])+" ("+Class[6]+") : ")
-            while group!='A' and group!='B':
+        if Class[5] > 1:
+            group = input(" - "+VerbList['askgroup'].format(Class[0]+" ("+Class[1]+")"+" | " +
+                                                            daysOfWeek[Class[2]].capitalize()+" "+Class[3]+" -> "+Class[4])+" ("+Class[6]+") : ")
+            while group != 'A' and group != 'B':
                 group = input(VerbList['errorAB'])
-            Groups.append([classes.index(Class),group])
+            Groups.append([classes.index(Class), group])
     return Groups
+
 
 def padTimeWithZeros(timeString):
     DPPos = timeString.index(':')
     b = timeString[0:DPPos]
-    if int(b)<10 and timeString[0]!='0':
-        timeString='0'+timeString
+    if int(b) < 10 and timeString[0] != '0':
+        timeString = '0'+timeString
     return timeString
 
+
 def ReadTxt(filename):
-    regex_dict = {
-    'firstline' : '^UV\s*Groupe\s*Jour\s*Début\s*Fin\s*Fréquence\s*Salle\(s\)\s*$',
-    'middleline' : '^\s*[A-Z0-9]{4}\s+([A-Z]\s+){0,1}(CM|TD|TP)\s[0-9]{1,2}\s+(lundi|mardi|mercredi|jeudi|vendredi|samedi)\s+([0-9]{1,2}:[0-9]{1,2}\s+){2}[1-2]\s+[A-Z]\s[0-9a-z]{3,4}\s*$'
-    }
-    regex = {} #dict containing compiled regexs
-    for key in regex_dict:
-        regex[key] = re.compile(regex_dict[key])
-    with open(filename,'r') as textfile:
+    with open(filename, 'r') as textfile:
         raw_lines = []
         for line in textfile:
-            raw_lines.append(line.replace('\t',' ').replace('\n',''))
+            raw_lines.append(line.replace('\t', ' ').replace('\n', ''))
         textfile.close()
     matchs = 0
     if regex['firstline'].match(raw_lines[0]):
-        matchs=1
+        matchs = 1
     else:
         print(VerbList['wrongheader'])
     for line in raw_lines[1:]:
         if regex['middleline'].match(line):
-            matchs+=1
+            matchs += 1
         else:
             print(VerbList['errline'].format(raw_lines.index(line)))
     if matchs != len(raw_lines):
         print(VerbList['requestreview'])
-        print("Nombre de matchs : ",matchs)
+        print("Nombre de matchs : ", matchs)
         return False
     print(VerbList['readsuccess'])
-    
+
     Classes = []
+
     for line in raw_lines[1:]:
-        newClass = line.replace('\t','').split(' ')
-        newClass = [value for value in newClass if value!='']
-        if not newClass[1] in ['CM','TD','TP']:
+        newClass = line.replace('\t', '').split(' ')
+        newClass = [value for value in newClass if value != '']
+        if not newClass[1] in ['CM', 'TD', 'TP']:
             newClass.pop(1)
         newClass.pop(2)
         newClass[6] = newClass[6]+newClass[7]
         newClass.pop(7)
-        newClass[2]=daysOfWeekFR.index(newClass[2])
-        newClass[5]=int(newClass[5])
+        newClass[2] = daysOfWeekFR.index(newClass[2])
+        newClass[5] = int(newClass[5])
         Classes.append(newClass)
     return Classes
+
+
+def findSemesterFile():
+    Candidates = [f for f in listdir('.') if path.isfile(f)]
+    finalCands = []
+    for cand in Candidates:
+        if regex['semesterFile'].match(cand):
+            finalCands.append(cand)
+    if not finalCands:
+        print(VerbList['noSemesterFile'])
+        return False
+    if len(finalCands) == 1:
+        return finalCands[0]
+    finalCands2 = []
+    for cand in finalCands:
+        finalCands2.append(cand[cand.index('_')+1:cand.index('.')])
+    select = input(VerbList['severalSemCals'].format(finalCands))
+    while not select in finalCands:
+        select = input(VerbList['wrongCode'])
+    return "SEM_"+select+".csv"
+
+
+def CheckSemester(filename):  # returns calendar min and max if
+    periods = []
+    with open(filename, 'r') as SemCal:
+        reader = csv.DictReader(SemCal, delimiter=';')
+        for row in reader:
+            startDTT = datetime.strptime(row['START'], '%d/%m/%y')
+            endDT = datetime.strptime(row['END'], '%d/%m/%y')
+            if startDTT > endDT or row['TYPE'] not in ['A', 'B']:
+                return False
+            periods.append([startDTT, endDT, row['TYPE']])
+    return periods
+
+
+def overlappingTwoPeriods(start_a, end_a, start_b, end_b):
+    return start_b <= end_a or start_a<=end_b
+
+def overlappingPeriods(periods):
+    for period in periods:
+        for period2 in periods[periods.index(period)+1:]:
+            if overlappingTwoPeriods(period[0],period[1],period2[0],period2[1]):
+                return False
+    return True
+
+def getGroupOfClass(groups, class_index):
+    for group in groups:
+        if group[0]==class_index:
+            return group[1]
+    return False
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+def CreateCalendar(groups, Classes, periods):
+    cal = Calendar()
+    dateFormat = "%Y-%m-%d %H:%M:%S"
+    for per in periods:
+        print(per)
+    for period in periods:
+        FirstWeekDay = period[0].weekday()
+        LastWeekDay = period[1].weekday()
+        for i in range(FirstWeekDay,LastWeekDay+1):
+            for Class in Classes:
+                if Class[2]==i:
+                    if Class[5]==1 or getGroupOfClass(groups,Classes.index(Class))==period[2]: #if frequency is 1 (class occuring each week) or if group matches period
+                        e = Event()
+                        startDT = period[0]+timedelta(Class[2]-period[0].weekday())
+                        startDT = startDT.replace(hour=int(Class[3][:Class[3].index(':')]))
+                        startDT = startDT.replace(
+                            minute=int(Class[3][Class[3].index(':')+1:]))
+                        endDT = startDT.replace(hour=int(Class[4][:Class[4].index(':')]))
+                        endDT = endDT.replace(minute=int(Class[4][Class[4].index(':')+1:]))
+                        e.begin = startDT.strftime(dateFormat)
+                        e.end = endDT.strftime(dateFormat)
+                        e.location = Class[6]
+                        e.name = Class[0]+" ("+Class[1]+")"
+                        cal.events.add(e)          
+    return cal
+
+def PrintReport(cal): #prints a brief report with number of events created
+    pass
 
 if __name__ == "__main__":
     print("**** UTBM PlanningToICS Script ****\nAuthor:\n")
     print("| | | |/ _/ | || |  V  | \n| |_| | \_| | \/ | \_/ | \n|___|_|\__/_|\__/|_| |_| \n\
     ")
     print("Version 1.0")
-    SelectedLang = input("Choose your language - Selectionne ta langue :\n-FR\n-EN\n : ")
+    SelectedLang = input(
+        "Choose your language - Selectionne ta langue :\n-FR\n-EN\n : ")
     SelectedLang = SelectedLang.lower()
     while(SelectedLang != "fr" and SelectedLang != "en"):
-        SelectedLang = input("Oops ! Please type \"EN\" or \"FR\" and press ENTER - Merci de saisir \"EN\" ou \"FR\" et d'appuyer sur ENTREE : ")
+        SelectedLang = input(
+            "Oops ! Please type \"EN\" or \"FR\" and press ENTER - Merci de saisir \"EN\" ou \"FR\" et d'appuyer sur ENTREE : ")
         SelectedLang = SelectedLang.lower()
-    if SelectedLang=="fr":
+    if SelectedLang == "fr":
         VerbList = frStrings
         daysOfWeek = daysOfWeekFR
-    elif SelectedLang=="en":
+    elif SelectedLang == "en":
         VerbList = enStrings
         daysOfWeek = daysOfWeekEN
 
@@ -166,60 +263,28 @@ if __name__ == "__main__":
     filename = input(VerbList['askfile'])
     while not validateFile(filename):
         filename = input(VerbList['askfile'])
-        
+
     Classes = ReadTxt(filename)
 
     if not Classes:
         exit(0)
 
-    #Groups = askGroups(Classes)
+    Groups = askGroups(Classes)
+
+    SemFile = findSemesterFile()
+    if not SemFile:
+        exit(0)
+    periods = CheckSemester(SemFile)
+    if not periods:
+        print(VerbList['errorPeriods'])
+        exit(0)
+    if overlappingPeriods(periods):
+        print(VerbList['overlappingPeriods'])
+        exit(0)
+
+    FinalCalendar = CreateCalendar(Groups, Classes, periods)
+
+    with open("output.ics", 'w') as ICSFIle:
+        ICSFIle.writelines(FinalCalendar)
     
-    c = Calendar()
-
-    GlobalStartDate = datetime(2020,9,14,8,0,0)
-
-    dateFormat = "%Y-%m-%d %H:%M:%S"
-
-    for Class in Classes:
-        e = Event()
-        startDate = GlobalStartDate+timedelta(Class[2])
-        startDate = startDate.replace(hour = int(Class[3][:Class[3].index(':')]))
-        startDate = startDate.replace(minute = int(Class[3][Class[3].index(':')+1:]))
-        endDate = startDate.replace(hour = int(Class[4][:Class[4].index(':')]))
-        endDate = endDate.replace(minute = int(Class[4][Class[4].index(':')+1:]))
-        e.begin = startDate.strftime(dateFormat)
-        e.end = endDate.strftime(dateFormat)
-        e.location = Class[6]
-        e.name = Class[0]+" ("+Class[1]+")"
-        c.events.add(e)
-    
-    with open("test.ics", 'w') as ICSFIle:
-        ICSFIle.writelines(c)
     print("DONE")
-
-
-'''
-c = Calendar()
-e = Event()
-'''
-
-
-    
-'''
-start_date = datetime.now()+timedelta(hours=1)
-Duration = timedelta(hours=1,minutes=30)
-
-e.name = "Test PythonToICS"
-e.begin = start_date.strftime("%Y-%m-%d %H:%M:%S")
-
-e.duration = Duration
-e.location = "P245"
-e.description = "Ceci est la description de cet evenement test"
-
-c.events.add(e)
-'''
-""" 
-with open("test.ics", 'w') as ICSFIle:
-    ICSFIle.writelines(c)
-print("DONE")
- """
--- README.md
+++ README.md
@@ -1,24 +1,73 @@
 # UTBMPlanningToICS script #
 
 ## Author ##
-```
-| | | |/ _/ | || |  V  |
-| |_| | \_| | \/ | \_/ |
-|___|_|\__/_|\__/|_| |_|
-```
+Valentin Mercy - Licium
 
 ## Script description ##
-UTBMPlanningToICS aims to simplify classes planning understanding for UTBM's students (UTBM = University of Technology of Belfort-Montbeliard). More precisely, this python script allows all UTBM students to turn the unformatted list of courses (which can be found on [their personnal space](https://monespace.utbm.fr/)) into a proper ICS file they can directly import on their favourite schedule utility, like Google Agenda for example.
+UTBMPlanningToICS aims to simplify classes planning understanding for UTBM's students (UTBM = University of Technology of Belfort-Montbeliard). More precisely, this python script allows all UTBM students to turn the unformatted list of courses (which can be found on [their personnal space](https://monespace.utbm.fr/)) into a proper ICS file they can directly import on their favourite schedule utility, such as Google Agenda for example.
 
 ## Supported languages ##
 * English
 * French
 
 ## How to use it ##
-
+* First, open a bash terminal, clone this repository and change your working directory to go inside :
+```
+git clone https://github.com/Valentin68/UTBMPlanningToICS.git
+cd UTBMPlanningToICS/
+```
+* Go to your [UTBM personnal space](https://monespace.utbm.fr/), navigate to "Mon dossier étudiant" > "Emploi du temps" and copy (Ctrl-C) your list of classes **with the header**. Please ensure that your selection starts just before the word "UV" and ends just after the location of the very last class.
+* Paste it into a new .txt file. That file must finally contain something like this :
+```
+UV 		Groupe 	Jour 	Début 	Fin 	Fréquence 	Salle(s)
+LO21 		TD 1 	lundi 	8:00 	10:00 	1 	P 130
+LC00 		TD 1 	lundi 	14:00 	16:00 	1 	P 122
+IFD1 		CM 1 	lundi 	16:15 	18:15 	1 	T 305
+PS22 		TD 2 	mardi 	8:00 	10:00 	1 	P 243
+MDA1 	A 	TD 1 	mardi 	14:00 	16:00 	1 	P 323
+MTC7 		TD 2 	mardi 	16:15 	18:15 	1 	P 325
+MDA1 	A 	CM 1 	mercredi 	8:30 	10:00 	1 	P 108b
+LO21 		CM 1 	mercredi 	14:00 	16:00 	1 	T 306
+LO21 		TP 2 	mercredi 	16:15 	19:15 	2 	P 334
+MTC7 		TD 2 	jeudi 	8:00 	10:00 	1 	P 122
+PS22 		CM 1 	jeudi 	10:15 	12:15 	1 	P 108b
+PS22 		TP 2 	vendredi 	8:00 	10:00 	2 	P 226
+IFD1 		CM 1 	vendredi 	10:15 	12:15 	1 	T 305
+MTC7 		CM 1 	vendredi 	14:00 	16:00 	1 	P 243
+LC00 		TP 4 	vendredi 	16:15 	17:15 	1 	P 145
+```
+* Save this file into the working directory, that should also contain the main script ```PlanningToICS.py``` and the list of A/B weeks for the next semester : ```SEM_A20.csv``` for example.
+* Launch the main script :
+```
+python PlanningToICS.py
+```
+* Follow the instructions
+* You should now have a new file named ```output.ics``` in your working directory. You can directly import it in your Google Agenda for example, and enjoy the result !
 
 ## How to contribute ##
 Feel free to contribute to this project ! Here are some possible improvments :
 * Create a GUI
 * Turn it into a standalone utility, maybe a portable .exe for Windows users
-* Use RRULES for repeating events
+* Use ICS RRULES for repeating events to link them together
+* Each semester, as the UTBM administration gives us the semester calendar, feel free to turn it into a SEM_XXX.csv file that can be read by this script. Please respect the following structure (only working periods must be listed) :
+```
+START;END;TYPE
+14/09/20;21/09/20;A
+21/09/20;28/09/20;B
+28/09/20;05/10/20;A
+05/10/20;12/10/20;B
+12/10/20;19/10/20;A
+19/10/20;26/10/20;B
+02/11/20;09/11/20;A
+09/11/20;16/11/20;B
+16/11/20;17/11/20;A
+18/11/20;18/11/20;B
+19/11/20;22/11/20;A
+23/11/20;29/11/20;B
+29/11/20;06/12/20;A
+07/12/20;14/12/20;B
+14/12/20;20/12/20;A
+04/01/21;06/01/21;B
+07/01/21;07/01/21;A
+08/01/21;08/01/21;B
+```
